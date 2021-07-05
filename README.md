7ba2358fdffb3a6fbfcbcccab9600cbda45dc268
Tim Roth <timdroth@gmail.com>
2021-07-05 11:37:32 -0700

    added Joe Biden ice cream-eating photo on submit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+Neighbor Number
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
 // Business Logic
+//function neighborNumber(string) {
+  //let numberArray = string.split(" ");
+  //return numberArray;
+//}
+//const numberArray = [6, 8, 10]
+//function numerize(numberArray) {
+  //return numberArray.parseInt;
+//}
 
 
 
 // UI Logic
 
 $(document).ready(function() {
-  $("form#codingQuiz").submit(function(event) {
+  $("#submit").click(function() {
+    $("#biden").show();
     event.preventDefault();
+  });
+  $("#reset").click(function() {
+    $("#biden").hide();
+    $("form#userNumbers").reset();
+    event.preventDefault();
+  });
+  $("form#userNumbers").submit(function(event) {
+    event.preventDefault();
+    
+    const numberArray = str.split("#userNumbers".val());
+    numberArray.push("#conversions");
+  });
+});  
